c0b65113e45d85c115571b7053c32cedd40d5bc6
Zach Ferguson <ferguszo@dukes.jmu.edu>
2026-03-25 20:47:44 -0400

    Runnable off of app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_folder=os.path.join(os.path.dirname(__file__), 'page', 'static'),
+            static_url_path='/static',
+            template_folder=os.path.join(os.path.dirname(__file__), 'page', 'templates'))
 
 @app.route('/')
 def index():
-    
+    return render_template('base.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
